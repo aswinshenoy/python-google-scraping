2ce112e2c81816556d0eb1ca3c23f185f9832313
Ashwin Shenoy <aswinshenoy65@gmail.com>
2018-08-23 18:11:09 +0000

    User query is set as parameter
    User search keyword is now set as parameter for requests.get(), instead of string concatenating  the url and the search term. Potential confusion between the words Query and Search Keyword has been fixed. Few comments have been added/modified.

--- google_scraping.py
+++ google_scraping.py
@@ -1,15 +1,20 @@
 import requests		# Requests required to obtain webpage
 import bs4		# BS4 required to parse webpage
 
+#Title Text Block
 print("GOOGLE SCRAPING USING PY3 AND BS4 \n")
 
-#User Can Enter Search Query
-query = input("ENTER SEARCH QUERY: ")
+#Accepts Search Keywords from the User
+keyword = input("ENTER SEARCH Keywords: ")
 
-url = "https://google.co.in/search?q=" + query;
+#Setting Query Parameters for Search 
+params = [('q',keyword)]
 
-# Gets response from the server
-response = requests.get(url)
+#Google Search URL
+url = "https://google.co.in/search"
+
+# Gets response from the server for the search query
+response = requests.get(url=url, params=params)
 
 # Response text from server is parsed using bs4
 soup = bs4.BeautifulSoup(response.text,'lxml')
@@ -17,11 +22,11 @@ soup = bs4.BeautifulSoup(response.text,'lxml')
 # Titles of results are found (Assumed that Google put h3 only on result titles)
 titles = soup.find_all("h3")
 
-print("\nDISPLAYING FIRST PAGE RESULT TITLES FOR " + query + "\n")
+#Result is Displayed
+
+print("\nDISPLAYING FIRST PAGE RESULT TITLES FOR " + keyword + "\n")
 
 x = 0; # counter variable
 for title in titles: #loop to display titles
 	x=x+1 # counter updated
 	print( str(x) + ". " + title.get_text() )
-
-
